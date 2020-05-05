e38fd6ad7f51c4ccec06c63a87fa65665577cfd0
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-05 16:58:26 -0600

    Add additional condition to if statment to render flash message if visitor tries adding video to bookmarks

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -3,12 +3,14 @@ class UserVideosController < ApplicationController
 
   def create
     user_video = UserVideo.new(user_video_params)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
+    if current_user.nil?
+      flash[:error] = 'You must be logged in to add to Bookmarks'
+      redirect_to(login_path) && return
+    elsif current_user.user_videos.find_by(video_id: user_video.video_id)
       flash[:error] = 'Already in your bookmarks'
     elsif user_video.save
       flash[:success] = 'Bookmark added to your dashboard!'
     end
-
     redirect_back(fallback_location: root_path)
   end
 
