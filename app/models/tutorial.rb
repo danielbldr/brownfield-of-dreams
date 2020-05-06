a5e453c67fb0a072010fefd43b87d20a8aa0839e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-05 20:40:37 -0600

    Add validations to tutorial model

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -5,4 +5,8 @@ class Tutorial < ApplicationRecord
            dependent: :destroy
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :thumbnail, presence: true
 end
