b7f9ee701da22c42fce4816de10bcd7e5c51a2e2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-05 20:40:11 -0600

    Create test for adding new tutorials inc sad path

--- spec/features/admin/admin_add_new_tutorials_spec.rb
+++ spec/features/admin/admin_add_new_tutorials_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 describe 'An admin user can create new tutorials' do
+  before(:each) do
+    @admin = create(:user, role: 1)
+    @tutorial = create(:tutorial)
+    @video1 = create(:video, tutorial_id: @tutorial.id)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
   it 'clicks on the add tutorial button' do
-    admin = create(:user, role: 1)
-    tutorial = create(:tutorial)
-    video1 = create(:video, tutorial_id: tutorial.id)
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
     visit "/admin/dashboard"
 
     click_link "New Tutorial"
@@ -15,9 +16,48 @@ describe 'An admin user can create new tutorials' do
     expect(current_path).to eq('/admin/tutorials/new')
 
     fill_in "Title", with: "How to make toast"
-    fill_in "Description", with: "Its not just for dinner!"
+    fill_in "Description", with: "It's not just for dinner!"
     fill_in "Thumbnail", with: "https://img.youtube.com/vi/pX0eJBvBNzs/maxresdefault.jpg"
 
     click_button "Save"
+
+    expect(current_path).to eq("/tutorials/#{Tutorial.all.last.id}")
+
+    expect(page).to have_content("Successfully created tutorial.")
+
+    within(".tutorials") do
+      expect(page).to have_content("How to make toast")
+    end
+  end
+
+  it 'must be a properly filled out form' do
+    visit "/admin/tutorials/new"
+
+    fill_in "Description", with: "It's not just for dinner!"
+    fill_in "Thumbnail", with: "https://img.youtube.com/vi/pX0eJBvBNzs/maxresdefault.jpg"
+
+    click_button "Save"
+
+    expect(current_path).to eq("/admin/tutorials/new")
+
+    expect(page).to have_content("Title can't be blank")
+
+    fill_in "Title", with: "How to make toast"
+    fill_in "Thumbnail", with: "https://img.youtube.com/vi/pX0eJBvBNzs/maxresdefault.jpg"
+
+    click_button "Save"
+
+    expect(current_path).to eq("/admin/tutorials/new")
+
+    expect(page).to have_content("Description can't be blank")
+
+    fill_in "Title", with: "How to make toast"
+    fill_in "Description", with: "It's not just for dinner!"
+
+    click_button "Save"
+
+    expect(current_path).to eq("/admin/tutorials/new")
+
+    expect(page).to have_content("Thumbnail can't be blank")
   end
 end
