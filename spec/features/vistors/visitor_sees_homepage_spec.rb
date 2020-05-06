0314527813daa3e05ac24c1a7af7ca2aedc48c63
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-05 20:07:09 -0600

    Add view logic to only render tutorials to a visitor that are not classroom content specific

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -2,24 +2,32 @@ require 'rails_helper'
 
 describe 'Visitor' do
   describe 'on the home page' do
-    it 'can see a list of tutorials' do
+    it 'can see a list of tutorials not marked as classroom content only' do
       tutorial1 = create(:tutorial)
       tutorial2 = create(:tutorial)
+      tutorial3 = create(:tutorial, classroom: true)
 
       video1 = create(:video, tutorial_id: tutorial1.id)
       video2 = create(:video, tutorial_id: tutorial1.id)
       video3 = create(:video, tutorial_id: tutorial2.id)
       video4 = create(:video, tutorial_id: tutorial2.id)
+      video5 = create(:video, tutorial_id: tutorial3.id)
+      video6 = create(:video, tutorial_id: tutorial3.id)
 
       visit root_path
 
       expect(page).to have_css('.tutorial', count: 2)
 
-      within(first('.tutorials')) do
-        expect(page).to have_css('.tutorial')
+      within("#tutorial-#{tutorial1.id}") do
         expect(page).to have_css('.tutorial-description')
         expect(page).to have_content(tutorial1.title)
         expect(page).to have_content(tutorial1.description)
+      end
+
+      within("#tutorial-#{tutorial2.id}") do
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial2.title)
+        expect(page).to have_content(tutorial2.description)
       end
     end
   end
