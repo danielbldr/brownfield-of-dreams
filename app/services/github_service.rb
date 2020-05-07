c0ea38022521376a292dc201bd9089e0f6c67ba3
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-07 16:11:04 -0600

    Create user_followers method to return follower json info

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,6 +4,11 @@ class GithubService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def user_followers(user_token)
+    response = conn(user_token).get('user/followers')
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def conn(user_token)
     Faraday.new(url: 'https://api.github.com') do |faraday|
       faraday.headers['Authorization'] = "token #{user_token}"
