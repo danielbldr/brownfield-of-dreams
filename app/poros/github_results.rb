2bb866c018b8ef7e5c8ed8ba6e58206c8192e770
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-07 16:13:14 -0600

    Create method get_followers to return followers from github api

--- app/poros/github_results.rb
+++ app/poros/github_results.rb
@@ -4,4 +4,10 @@ class GithubResults
     json = github_service.user_repos(token)
     json.map { |repo| Repo.new(repo) }
   end
+
+  def get_followers(token)
+    github_service = GithubService.new
+    json = github_service.user_followers(token)
+    json.map { |repo| Follower.new(repo) }
+  end
 end
