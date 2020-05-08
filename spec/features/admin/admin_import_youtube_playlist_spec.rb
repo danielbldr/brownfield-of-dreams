46c3b9c744d55a43d6ab37da59fd4d42377c8368
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-07 21:06:12 -0600

    playing with importing playlists

---
+++ spec/features/admin/admin_import_youtube_playlist_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'As an admin I can click new tutorial on the dashboard' do
+  describe 'I can then click Import Youtube Playlist' do
+    it 'I see a form and can enter a valid Youtube Playlist id to create tutorial' do
+      admin = create(:user, role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit '/admin/dashboard'
+      click_link 'New Tutorial'
+
+      fill_in "Title", with: "Ashtanga with Mark Robberds"
+      fill_in "Description", with: "Learn about traditional ashtanga yoga"
+      fill_in "Thumbnail", with: "https://img.youtube.com/vi/frVPA_mLXh4/maxresdefault.jpg"
+
+      click_link 'Import YouTube Playlist'
+
+      fill_in 'Playlist ID', with: "PLpfKu0U8zxt5rvqkZz8C6NDtjuXyl4HwW"
+
+      click_button 'Save'
+    end
+  end
+end
