5f4fb5cea8f90825d7d4c3e8619f7eba873b303c
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-08 16:34:56 -0600

    Add create action to GithubController to save github user access token

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,5 +1,12 @@
 class GithubController < ApplicationController
   def create
-    require "pry"; binding.pry
+    token = auth_info['credentials']['token']
+    current_user.update_attribute(:token, token)
+    redirect_to dashboard_path
+  end
+
+  protected
+  def auth_info
+    request.env['omniauth.auth']
   end
 end
