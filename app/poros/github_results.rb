83eaf0c74256dc6e2295334a5ea90479095103f7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-07 20:23:40 -0600

    Update github results poro to be able to retrieve following users info

--- app/poros/github_results.rb
+++ app/poros/github_results.rb
@@ -4,4 +4,10 @@ class GithubResults
     json = github_service.user_repos(token)
     json.map { |repo| Repo.new(repo) }
   end
+
+  def get_following(token)
+    github_service = GithubService.new
+    json = github_service.user_following(token)
+    json.map { |following_user_info| Following.new(following_user_info)}
+  end
 end
