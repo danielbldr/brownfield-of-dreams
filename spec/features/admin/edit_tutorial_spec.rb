d144829e9fe8d756c58326a5729b80fcc1eaf85e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-09 11:14:01 -0600

    Create sad path for video not being created

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe "An Admin can edit a tutorial" do
+
   let(:tutorial) { create(:tutorial) }
   let(:admin)    { create(:admin) }
 
@@ -21,5 +22,23 @@ describe "An Admin can edit a tutorial" do
     within(first(".video")) do
       expect(page).to have_content("How to tie your shoes.")
     end
+  end
+
+  scenario "videos must have a title and an ID" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit edit_admin_tutorial_path(tutorial)
+
+    click_on "Add Video"
+
+    fill_in "video[description]", with: "Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through."
+    fill_in "video[video_id]", with: "J7ikFUlkP_k"
+    click_on "Create Video"
+
+    expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
+
+    expect(page).to have_content("Unable to create video.")
+
+    expect(page).to_not have_css(".video")
   end
 end
