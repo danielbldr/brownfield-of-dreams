9d8df66c14c07d16e96c2b027565db7c17821ffb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-09 11:14:13 -0600

    Update controller with flash sad path

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -9,19 +9,15 @@ class Admin::VideosController < Admin::BaseController
   end
 
   def create
-    begin
       tutorial = Tutorial.find(params[:tutorial_id])
       thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
       video = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
-
-      video.save
-
-      flash[:success] = 'Successfully created video.'
-    rescue StandardError
-      flash[:error] = 'Unable to create video.'
-    end
-
-    redirect_to edit_admin_tutorial_path(id: tutorial.id)
+      if video.save
+        flash[:success] = 'Successfully created video.'
+      else
+        flash[:error] = 'Unable to create video.'
+      end
+      redirect_to edit_admin_tutorial_path(id: tutorial.id)
   end
 
   private
