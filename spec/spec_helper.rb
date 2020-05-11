ff6c57d1d6305a6ebce0f39ab17ee29f91f13e4f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-11 15:50:10 -0600

    Add params for webmcok and vcr to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
+require 'webmock/rspec'
 RSpec.configure do |config|
+  require 'vcr'
+
+  VCR.configure do |c|
+    c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+    c.hook_into :webmock
+    c.filter_sensitive_data('<GITHUB_API_KEY>') { ENV['GITHUB_API_KEY'] }
+    c.configure_rspec_metadata!
+  end
 
   config.before(:suite) do
      DatabaseCleaner.clean_with(:truncation)
