f17ea5488e5656208b0c9d6f70722711c64d8875
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-11 20:43:19 -0600

    Add functionality to add GH username to database

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,7 +1,7 @@
 class GithubController < ApplicationController
   def create
     token = auth_info['credentials']['token']
-    current_user.update(token: token)
+    current_user.update(token: token, github_login: get_user_login(token))
     redirect_to dashboard_path
   end
 
@@ -9,5 +9,10 @@ class GithubController < ApplicationController
 
   def auth_info
     request.env['omniauth.auth']
+  end
+
+  def get_user_login(token)
+    github = GithubService.new
+    login = github.user_repos(token).first[:owner][:login]
   end
 end
