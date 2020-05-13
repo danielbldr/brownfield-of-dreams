8cbf95f90c69af5fbef2d2bd3f97f59e4c05e72f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-12 21:02:03 -0600

    Add has_many to user model along with not_yet_friends method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :videos, through: :user_videos
+  has_many :friends
+  has_many :friended, through: :friends
 
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
@@ -11,5 +13,12 @@ class User < ApplicationRecord
 
   def self.in_database?(username)
     User.where(github_login: username).exists? && !username.nil?
+  end
+
+  def not_yet_friends?(follower)
+    friend = User.where(github_login: follower.login).first
+    return false if friend.friended.include?(self)
+
+    true
   end
 end
