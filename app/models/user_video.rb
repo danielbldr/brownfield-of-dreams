8c19b6cfb2f4ae2327ad6eec91332ce708a648a6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-13 16:27:18 -0600

    Update DB query to only include that users bookmarked videos

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -3,7 +3,8 @@ class UserVideo < ApplicationRecord
   belongs_to :user
 
   def self.bookmarked_videos(user_id)
-    require "pry"; binding.pry
-    Video.joins(%I[tutorial user_videos]).order(:tutorial_id, :position)
+    Video.joins(:user_videos)
+         .where('user_videos.user_id = ?', user_id)
+         .order(:tutorial_id, :position)
   end
 end
