cfde171117549fa0ac760b5684d8b0ad97c1db1e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-13 13:15:00 -0600

    Create method to return bookmarks ordered by tutorial and position

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,4 +1,8 @@
 class UserVideo < ApplicationRecord
   belongs_to :video
   belongs_to :user
+
+  def self.bookmarked_vidoes
+    Video.joins(%I[tutorial user_videos]).order(:tutorial_id, :position)
+  end
 end
