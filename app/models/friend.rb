b4160790da02c1e00aa363c80f8421f3780f54a0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-12 20:59:50 -0600

    Add friend model

---
+++ app/models/friend.rb
@@ -0,0 +1,12 @@
+class Friend < ApplicationRecord
+  belongs_to :user
+  belongs_to :friended,  class_name: "User"
+
+  after_create :create_inverse
+
+  def create_inverse
+    self.class.create(user_id: self.friended.id, friended_id: self.user.id)
+  end
+
+  validates_uniqueness_of :user_id, scope: :friended_id
+end
