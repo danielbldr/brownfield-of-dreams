5002c25aba2f2885e0d13d9c96541b33c90eb4de
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-13 16:00:32 -0600

    Add user id to bookmark search method

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -2,7 +2,8 @@ class UserVideo < ApplicationRecord
   belongs_to :video
   belongs_to :user
 
-  def self.bookmarked_videos
+  def self.bookmarked_videos(user_id)
+    require "pry"; binding.pry
     Video.joins(%I[tutorial user_videos]).order(:tutorial_id, :position)
   end
 end
