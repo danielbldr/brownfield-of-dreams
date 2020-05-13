d4bd75e5f49c46d8834e5c2abb1d13ee0be32110
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-13 14:20:03 -0600

    Save @tutorial.videos as @tutorial_videos so I can add pagination

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,6 +2,7 @@ class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
+    @tutorial_videos = @facade.videos.paginate(page: params[:page], per_page: 5)
     @tutorial = Tutorial.last
   end
 end
