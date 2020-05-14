b6cc8b3a62a24e7fac99b6a08f1f9709f042e0ed
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-13 21:58:35 -0600

    Update production env settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    domain: 'heroku.com',
+    address:        "smtp.sendgrid.net",
+    port:            587,
+    authentication: :plain,
+    user_name:      'apikey',
+    password:       ENV['SENDGRID_API_KEY']
+  }
 end
