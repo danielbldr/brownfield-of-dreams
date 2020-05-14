921561aa57f6ca6cb1636bb21c3ab3142dd2dcd4
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-14 09:56:35 -0600

    Update name in email

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -1,7 +1,7 @@
 class ActivationMailer < ApplicationMailer
   def inform(email_info, user_email)
-    @user_name = email_info[:user]
+    @user_name = email_info[:user][:first_name]
     @user_id = email_info[:user].id
-    mail(to: user_email, subject: "#{@user} - please activate account")
+    mail(to: user_email, subject: "#{@user_name} - please activate account")
   end
 end
